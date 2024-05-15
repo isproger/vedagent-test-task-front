ac2825c9b39f8f9e18d5a03d2111088a26e59af3
isproger <bestversyou@gmail.com>
2024-05-15 18:18:15 +0700

    refactoring

---
+++ src/types.ts
@@ -0,0 +1,59 @@
+import { AxiosError } from "axios"
+
+export interface IFetchActionProps{
+    success: Function
+    error?: Function
+    finaly?: Function
+}
+
+export interface ISelectOption{
+    value: number
+    label: string
+}
+
+
+
+export interface IFormData{
+    transportCompany: number | null
+    productType: number | null
+    weight: number | null
+    distance: number | null
+}
+
+export interface IFormSelectOptionResponse{
+    data: ISelectOption[]
+}
+
+export interface IFormCalculateResponse{
+    value: number
+    print_value?: string
+}
+
+export interface IFormSaveResponse{
+    calculation?: {
+        transport_company_id: number
+        product_type_id: number
+        weight_kg: number
+        distance_km: number
+        summ: number
+        updated_at: Date
+        created_at: Date
+        id: number
+    }
+    message?: string
+    error?: string
+    [k: string]: any
+}
+
+export interface IFormCalculateResult extends IFormCalculateResponse{}
+
+export interface IDataProps{
+    isLoading: boolean
+    isValidationEnabled: boolean
+    result: IFormCalculateResult,
+    transportCompanies: ISelectOption[], 
+    productTypes: ISelectOption[],  
+    formData: IFormData,
+    error: null | AxiosError 
+    message: null | string
+}
